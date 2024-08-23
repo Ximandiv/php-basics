cbb5769df6f77761792612a01772ab70e0972b44
Ximandiv <ximandiv@gmail.com>
2024-08-23 15:09:13 -0500

    Initial

---
+++ PHP-Reinforce-Basics/index.php
@@ -0,0 +1,63 @@
+<?php
+
+$amIAtWork = true;
+
+if($amIAtWork){
+    echo "If you see this, I'm at work!";
+}
+else{
+    echo "If you see this, I'm not at work!";
+}
+
+function greetPerson($greet, $person)
+{
+    return "{$greet()} {$person()}!";
+}
+
+$supGreet = function(){
+    return "Sup'";
+};
+
+$helloGreet = function(){
+    return "Hello";
+};
+
+$personName = function(){
+    return 'Taka';
+};
+
+$world = function(){
+    return 'World';
+};
+
+$favoritePeopleList = ['Taka', 'Mr. Poof', 'Chambas', 'Julian'];
+
+$favPeopleWDetailsList =
+    [
+      [
+          "name" => "Taka",
+          "age" => 20,
+          "gender" => "Male",
+          "job" => "Web Developer"
+      ],
+        [
+            "name" => "Mr. Poof",
+            "age" => 24,
+            "gender" => "Male",
+            "job" => "Backend Developer"
+        ],
+        [
+            "name" => "Chambas",
+            "age" => 35,
+            "gender" => "Male",
+            "job" => "Fullstack Senior Web Developer"
+        ],
+        [
+            "name" => "Julian",
+            "age" => 38,
+            "gender" => "Male",
+            "job" => "Human Resources Director"
+        ]
+    ];
+
+require 'index.view.php';
