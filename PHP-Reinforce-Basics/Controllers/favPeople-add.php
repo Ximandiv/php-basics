67290d3e2ab70b2a35eacb46f88b30483e590a9e
Emmanuel Vanegas Carmona <119233001+Ximandiv@users.noreply.github.com>
2024-09-02 08:38:24 -0500

    Clean and prepare DB Command for POST on Fav People

--- PHP-Reinforce-Basics/Controllers/favPeople-add.php
+++ PHP-Reinforce-Basics/Controllers/favPeople-add.php
@@ -5,7 +5,17 @@ $title = 'Epic DB Title';
 $banner = 'Favorite People';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    dd($_POST);
+    $cleanInput = [];
+
+    foreach($_POST as $key => $value){
+        $cleanInput[$key] = strip_tags($value);
+    }
+    
+    dd($cleanInput);
+
+    $dbContext = new DBContext();
+
+    // Add SQL Command down here
 }
 
 require 'Views/favPeople-add.view.php';
